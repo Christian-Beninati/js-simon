ad42212045d87d970411cddf63162eb92493d567
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-09 21:06:35 +0100

    Simon-Say-Completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,123 @@
-console.log('JS OK')
+// console.log('JS OK')
+
+// Function---------------------------------------
+
+// Genero un numero casuale compreso tra min e max (inclusi)
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Genero un array di 5 numeri casuali diversi
+function generateRandomNumbers() {
+  const numbers = [];
+
+  while (numbers.length < 5) {
+    const randomNumber = generateRandomNumber(1, 100);
+
+    if (!numbers.includes(randomNumber)) {
+      numbers.push(randomNumber);
+    }
+  }
+
+  return numbers;
+}
+
+// Funzione che conta il numero di numeri indovinati
+function countGuessedNumbers(generatedNumbers, userNumbers) {
+  let count = 0;
+  const guessedNumbers = [];
+
+  for (const number of userNumbers) {
+    if (generatedNumbers.includes(number)) {
+      count++;
+      guessedNumbers.push(number);
+    }
+  }
+
+  return {
+    count,
+    guessedNumbers
+  };
+}
+
+// Game Logic-------------------------------------
+
+// Funzione principale del programma
+function play() {
+  // Genera i numeri casuali
+  const generatedNumbers = generateRandomNumbers();
+
+  // Recupero l'elemento dal DOM
+  const numbersContainer = document.getElementById("numbers-container");
+  // Rimuovo la classe hidden per mostrare i numeri quando riparte una nuova partita
+  numbersContainer.classList.remove("hidden"); 
+  let numbersString = '';
+
+  for (let i = 0; i < generatedNumbers.length; i++) {
+    numbersString += generatedNumbers[i];
+
+    if (i !== generatedNumbers.length - 1) {
+      numbersString += " - ";
+    }
+  }
+  
+  numbersContainer.innerText = numbersString;
+  // Tempo rimanente in secondi
+  let secondsLeft = 30;
+  
+  
+  // Funzione per aggiornare il timer
+  function updateTimer() {
+    console.log(`Tempo rimanente: ${secondsLeft} secondi`);
+
+    if (secondsLeft === 0) {
+      clearInterval(timer); // Ferma il timer
+
+      // Nascondo i numeri generati
+      numbersContainer.classList.add("hidden");
+
+      // Prompt per l'inserimento dei numeri 
+      setTimeout(() => {
+        const userNumbers = [];
+        for (let i = 0; i < 5; i++) {
+          let enteredNumber = parseInt(prompt("Inserisci un numero tra 1 e 100:"));
+          while (isNaN(enteredNumber)) {
+            enteredNumber = parseInt(prompt("Caratteri non validi! Inserisci un numero tra 1 e 100:"));
+          }
+          userNumbers.push(enteredNumber);
+        }
+
+        // Conto i numeri indovinati
+        const { count, guessedNumbers } = countGuessedNumbers(generatedNumbers, userNumbers);
+
+        // Mostro i numeri indovinati e il punteggio
+        if (count === 0) {
+          alert("Mi dispiace. Non hai indovinato nessun numero.");
+        } else if (count === 5) {
+          alert("Complimenti! Hai totalizzato 5 punti. Hai indovinato tutti i numeri.");
+        } else {
+          alert(`Hai totalizzato ${count} punti. Hai indovinato: ${guessedNumbers}.`);
+        }
+        
+        // Richiedo all'utente se vuole fare un'altra partita
+        const playAgain = confirm("Vuoi fare un'altra partita?");
+
+        if (playAgain) {
+        play(); // Avvio una nuova partita
+        } else {
+        alert("Grazie per aver giocato. A presto!");
+        }
+
+        
+      }); 
+    }
+
+    secondsLeft--;
+  }
+
+  // Aggiorno il timer ogni secondo
+  const timer = setInterval(updateTimer, 1000);
+}
+
+// Chiamata diretta alla funzione play()
+play();
--- steps.md
+++ steps.md
@@ -8,3 +8,21 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 <br>
 
 - inizio
+- Creo una funzione per generare un numero casuale da min a max (1 - 100)
+- Creo una funzione per generare un array di numeri casiali tutti diversi
+- Creo una funzione che tiene il conteggio dei numeri indovinati
+- Funzione principale del programma (play)
+  - Genero i numeri casuali utilizzando la funzione (fatta precedentemente)
+  - Recupero l'elementto dal Dom
+  - Rimuovo la classe 'hidden' dall'elemento per mostrare i numeri quando viene avviata una nuova partita.
+  - Creo una stringa che rappresenta i numeri generati e la inserisco nell'elemnto del DOM
+  - Avvio un timer di 30 secondi per la durata della partita.
+  - Aggiorno il timer ad ogni secondo.
+  - Se il tempo è scaduto, interrompo il timer e nascondo i numeri generati.
+  - Creo i Prompt per l'inserimento dei numeri da parte dell'utente.
+  - Conto i numeri indovinati utilizzando la funzione creata precedentemente.
+  - Mostro i numeri indovinati e il punteggio all'utente.
+  - Chiedo all'utente se desidera fare un'altra partita.
+    - Se l'utente conferma, avvio una nuova partita richiamando la funzione "play".
+    - Altriment mostro un messaggio di saluto e termino il programma.
+- Fine
